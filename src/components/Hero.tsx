4996cc057678fcb361198af764efbb4a849147ee
Snehil Gupta <snehilgupta97@gmail.com>
2025-11-09 21:06:43 +0530

    updated Projects

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -86,6 +86,13 @@ const Hero = () => {
           </div>
         </div>
       </div>
+
+      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce">
+        <div className="w-6 h-10 border-2 border-white/50 rounded-full flex items-start justify-center p-2">
+          <div className="w-1 h-3 bg-white/50 rounded-full"></div>
+        </div>
+      </div>
+      
     </section>
   );
 };
